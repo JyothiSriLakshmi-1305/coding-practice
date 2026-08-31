11311e670d4084efb90634a22398ac6eb8ea8bcb
Jyothi Sri Lakshmi Kuna <jyothikuna2005@gmail.com>
2026-08-31 11:12:42 +0530

    added TwoSum.java

---
+++ LeetCode/easy/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
+/*
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]*/
