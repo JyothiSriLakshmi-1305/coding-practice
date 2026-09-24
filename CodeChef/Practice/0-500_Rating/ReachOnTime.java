2bf53cf045ffa1baa410fad7dbf0671051ab3966
Jyothi Sri Lakshmi Kuna <jyothikuna2005@gmail.com>
2026-09-24 22:16:36 +0530

    Added ReachOnTime.java

---
+++ CodeChef/Practice/0-500_Rating/ReachOnTime.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class ReachOnTime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+
+        while(t-->0) {
+            int x=sc.nextInt();
+
+            if(x<=30) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+
+        sc.close();
+    }
+}
+/*
+Sample Input
+6
+30
+60
+14
+29
+31
+42
+Your Output
+Yes
+Yes
+No
+No
+Yes
+Yes*/
